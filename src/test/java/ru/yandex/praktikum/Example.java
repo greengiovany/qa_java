eed45abc7da52121a7cbeb576eaf84e7607e68eb
greenjo114 <106109560+greenjo114@users.noreply.github.com>
2022-06-06 15:34:41 +0300

    delete example file

--- src/test/java/ru/yandex/praktikum/Example.java
+++
@@ -1,8 +0,0 @@
-package ru.yandex.praktikum;
-
-public class Example {
-    public static void main(String[] args) {
-        Feline feline = new Feline();
-        Lion lion = new Lion(feline);
-    }
-}
